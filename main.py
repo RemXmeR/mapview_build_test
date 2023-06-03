e78d4d1b04c414fdb4cfc891cc4de79ba4a81479
RemXYZ <remkarablin20@gmail.com>
2023-06-03 19:28:00 +0200

    mapview_build

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-from kivy.garden.mapview import MapView
+from build.lib.kivy_garden.mapview import MapView
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
-# import os
-
-# kv_path = os.path.join(os.path.dirname(__file__), 'main.kv')
-# print(kv_path)
-# exit()
 kv = Builder.load_file("main.kv")
 
 class Places_Mapview(MapView):
@@ -15,27 +10,6 @@ class Places_Mapview(MapView):
 class MyLayout(Widget):
     def press(self):
         pass
-    #     notification.notify(
-    #         title="Hello",
-    #         message="100m od Ciebie jest Piękny Domek"
-    #     )
-    # def on_pause(self):
-    #     notification.notify(
-    #         title="PAUSE",
-    #         message="Paused"
-    #     )
-    #
-    # def on_resume(self):
-    #     notification.notify(
-    #         title="RESUME",
-    #         message="Resumed"
-    #     )
-    #
-    # def on_stop(self):
-    #     notification.notify(
-    #         title="STOP",
-    #         message="Stoped"
-    #     )
 
 class myApp(App):
     def build(self):
