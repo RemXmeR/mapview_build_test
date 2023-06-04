440b294d8af8dcb472643182c462d1d772c748af
RemXYZ <remkarablin20@gmail.com>
2023-06-04 12:22:13 +0200

    background work!

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ import os
 
 kv_path = os.path.join(os.path.dirname(__file__), 'main.kv')
 print(kv_path)
+
+
 # exit()
-# kv = Builder.load_file("main.kv")
+kv = Builder.load_file(kv_path)
 
 
 class Places_Mapview(MapView):
@@ -26,7 +28,7 @@ class MyLayout(Widget):
         notification.notify(
             title="Hello",
             message="100m od Ciebie jest Piękny Domek",
-            timeout=10
+            timeout=2
         )
 
 
@@ -35,6 +37,11 @@ class MyApp(App):
 
     def build(self):
         return MyLayout()
+
+    def on_start(self):
+        # Zadanie, które będzie uruchamiane w tle
+        pass
+        # Clock.schedule_interval(, 0)
 
     def on_pause(self):
         print("PAUSED")
@@ -73,12 +80,78 @@ class MyApp(App):
         )
 
 
+# async def print_hello_world():
+#     while True:
+#         print("Hello, world")
+#         await asyncio.sleep(1)  # Czeka na 1 sekundę
+
+# Utworzenie nowej pętli zdarzeń asyncio i dodanie do niej zadania
+# loop = asyncio.new_event_loop()
+# loop.create_task(print_hello_world())
+#
+# # Synchronizacja pętli zdarzeń asyncio z zegarem Kivy
+# loop.run_forever()
+
+# async def main():
+#     await MyApp().async_run(async_lib="asyncio")
+#
+# # Uruchomienie pętli zdarzeń asyncio
+#
+# loop = asyncio.new_event_loop()
+# loop.create_task(main())
+#
+# # Synchronizacja pętli zdarzeń asyncio z zegarem Kivy
+# loop.run_forever()
+# print("LOL XD")
+
+
+
+
+import threading
+import time
+
+def print_hello_world2():
+    while True:
+        print('Hello, world!')
+        time.sleep(1)  # Możesz dostosować tę wartość, aby kontrolować częstotliwość wyświetlania
+
+class HelloWorldPrinter:
+    def __init__(self):
+        self.stop_printing = threading.Event()
+
+    def print_hello_world(self):
+        while not self.stop_printing.is_set():
+            print('Hello, world!')
+            time.sleep(1)
+
+    def stop(self):
+        self.stop_printing.set()
+
+
+def run_main_code(printer):
+    for i in range(10):
+        print(f"Running main code: {i}")
+        time.sleep(1)
+    printer.stop()
+
+
+printer = HelloWorldPrinter()
+background_thread = threading.Thread(target=printer.print_hello_world)
+background_thread.start()
+
+MyApp().run()
+printer.stop()
+
+
+
+
 
 # Uruchomienie pętli zdarzeń asyncio
-loop = asyncio.get_event_loop()
-loop.run_until_complete(
-    async_runTouchApp(MyApp.run(), async_lib='asyncio'))
-loop.close()
+# MyApp().run()
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(
+#     async_runTouchApp(MyApp.run(), async_lib='asyncio'))
+# loop.close()
 # loop = asyncio.get_event_loop()
 # loop.run_until_complete(
 #     async_runTouchApp(myApp(), async_lib='asyncio'))
